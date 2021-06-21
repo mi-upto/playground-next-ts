f50851a04ad0f2748b9838e733f4e3bec2d03df6
mi <shibuta.misato@neko-note.me>
2021-06-21 12:16:40 +0900

    refactor: api func

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -4,10 +4,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 type Data = {
   name: string
 }
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+const hello = (req: NextApiRequest, res: NextApiResponse<Data>) => {
   res.status(200).json({ name: 'John Doe' })
 }
+
+export default hello;
