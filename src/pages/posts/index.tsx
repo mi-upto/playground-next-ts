5ad4fdcb281356f7bc4e50a8b953fe699b656c71
mi <shibuta.misato@neko-note.me>
2021-06-28 17:22:10 +0900

    [playground] staticProps, serverSideProps

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,12 @@
 import { LayoutDefault } from '@/components/layouts/default';
 import getSortedPostsData, { Post } from '@/lib/post';
 import type { NextPage } from 'next';
-import AppLink from '@/components/links/default';
+// import type { NextPage, GetServerSideProps } from 'next';
 
-export async function getStaticProps() {
+// ビルド時のサーバ側でのみ実行されクライアント側で実行されることは無い
+// ブラウザのJSバンドルには含まれ無い
+// @see https://nextjs.org/learn/basics/data-fetching/getstaticprops-details
+export const getStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
@@ -11,6 +14,23 @@ export async function getStaticProps() {
     },
   };
 }
+
+// リクエストたび SSR
+// @see https://nextjs.org/learn/basics/data-fetching/request-time
+// type Data = {
+//   id: number;   // dummy
+//   text: string; // dummy
+// };
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const res = await fetch('https://.../data');
+//   const data: Data = await res.json();
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
 
 type Props = {
   allPostsData: Post[]
