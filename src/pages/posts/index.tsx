78602ba6d2b2dca4f3e0d5e9754e939dd8d8cea0
mi <shibuta.misato@neko-note.me>
2021-06-28 16:26:46 +0900

    [playground] show markdown post list

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,34 @@
 import { LayoutDefault } from '@/components/layouts/default';
+import getSortedPostsData, { Post } from '@/lib/post';
 import type { NextPage } from 'next';
 import AppLink from '@/components/links/default';
 
-export const Posts: NextPage = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+type Props = {
+  allPostsData: Post[]
+};
+const Posts: NextPage<Props> = ({ allPostsData }) => {
   return (
     <LayoutDefault title="First Post">
-      <h1>First Post</h1>
-      <AppLink href="/">トップページ</AppLink>
+      <section>
+        <h1 className="text-3xl my-5">Blog Title</h1>
+        <ul>
+          {allPostsData.map(({ id, date, title }: Post) => (
+            <li key={id} className="flex flex-col mb-4">
+              <time className="text-gray-500 text-sm">{date}</time>
+              <h2>{title}</h2>
+            </li>
+          ))}
+        </ul>
+      </section>
     </LayoutDefault>
   );
 };
---
+++ src/posts/gray-matter.md
@@ -0,0 +1,15 @@
+---
+title: 'YAMLFront Matter'
+date: '2020-01-03'
+---
+
+# YAMLFront Matter
+
+```
+---
+title: 'YAMLFront Matter'
+date: '2020-01-03'
+---
+```
+
+You might have noticed that each markdown file has a metadata section at the top containing title and date. This is called YAML Front Matter, which can be parsed using a library called [gray-matter](https://github.com/jonschlinkert/gray-matter)
