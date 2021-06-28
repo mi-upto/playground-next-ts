ae6f87f0b5694cb6a75f0d7289ac22a985395074
mi <shibuta.misato@neko-note.me>
2021-06-28 18:04:59 +0900

    [playground] swr

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,23 +2,41 @@ import { LayoutDefault } from "@/components/layouts/default";
 import Link from 'next/link'
 import Image from 'next/image'
 import type { NextPage } from 'next';
+import useSWR from 'swr';
+
+function Profile() {
+
+
+
+  return <div></div>;
+}
 
 export const About: NextPage = () => {
+  const { data, error } = useSWR('https://dog.ceo/api/breeds/image/random', fetch);
+  if (error) {
+    return <div>failed to load</div>
+  }
+  if (!data) {
+    return <div>loading...</div>
+  }
+
+  console.log('data', data);
+
+
   return (
     <LayoutDefault>
       <h1 className="text-5xl">This is About Page</h1>
       <div className="my-4">
         Return <Link href="/">First page</Link>
       </div>
-
       <Image src="/images/profile.jpg" alt="profile" height={144} width={144} />
 
-      <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-4">
+      <div className="grid gap-4 grid-flow-col grid-cols-3 grid-rows-2">
         <div>1</div>
         <div className="col-start-3">2</div>
         <div>3</div>
         <div>4</div>
-        <div className="row-start-1 col-start-2 col-span-2">5</div>
+        <div className="col-span-2 col-start-2 row-start-1">5</div>
       </div>
     </LayoutDefault>
   );
